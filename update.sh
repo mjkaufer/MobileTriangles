06be22c6c1c31106bf0d5361179d7aae1cb630a4
Matthew Kaufer <matthew@kaufer.org>
2013-05-21 17:06:52 -0400

    Commit

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 git init
 git add .
-git commit -m "Commit plz"
+git commit -m "Commit"
 git config --global user.name "Matthew Kaufer"
 git config --global user.email matthew@kaufer.org
-git remote add origin https://github.com/mjkaufer/DesktopTriangles.git
+git remote add origin https://github.com/mjkaufer/MobileTriangles.git
 git push -u origin master
 
